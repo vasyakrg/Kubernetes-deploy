818e35b9139a762a9769efbe6c32893d85364141
Vassiliy Yegorov <vasyakrg@gmail.com>
2021-05-22 14:50:51 +0700

    fix

--- App/docker/db/build.sh
+++ App/docker/db/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker buildx build --platform linux/amd64 --push -t vasyakrg/php-db .
+
+# base
+# docker build -t vasyakrg/php-db .
+# docker push vasyakrg/php-db
--- App/docker/php/build.sh
+++ App/docker/php/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker buildx build --platform linux/amd64 --push -t vasyakrg/php-app .
+
+# base
+# docker build -t vasyakrg/php-db .
+# docker push vasyakrg/php-db
