0d2fff12feeeae79ec9da15d4e9c8804875ce062
Vassiliy Yegorov <vasyakrg@gmail.com>
2021-05-23 11:20:16 +0700

    fix data

--- Cluster/nfs/nfs-server.sh
+++ Cluster/nfs/nfs-server.sh
@@ -11,7 +11,6 @@ FOLDER=/mnt/data
 apt update && apt install -y nfs-kernel-server
 
 mkdir -p $FOLDER
-chmod 777 -R $FOLDER
 chown nobody:nogroup -R $FOLDER
 
 echo "$FOLDER ${NODE1}/24(rw,sync,no_subtree_check,no_root_squash) ${NODE2}/24(rw,sync,no_subtree_check,no_root_squash) ${NODE3}/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
