c3a2d72a52a0d4dc3d212c3f90fc5212e0b117e6
Vassiliy Yegorov <vasyakrg@gmail.com>
2021-05-23 10:43:57 +0700

    fix

--- Cluster/2-add-config.sh
+++ Cluster/2-add-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # add config file
-cat kube_config_cluster.yml > ~/.kube/k8s-hls
+cat kube_config_cluster.yml > ~/.kube/k8s-hls.yaml
 
 # enable config auth
 export KUBECONFIG=$(find ~/.kube -maxdepth 1 -type f -name '*' | tr "\n" ":")
